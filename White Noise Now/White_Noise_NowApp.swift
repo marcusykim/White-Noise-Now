fb498fead683c4ec1ccc254b1c0d3e1b997575a4
Marcus Kim <marcuskim1989@gmail.com>
2025-02-20 19:28:15 -0800

    Initial Commit

---
+++ White Noise Now/White_Noise_NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  White_Noise_NowApp.swift
+//  White Noise Now
+//
+//  Created by Marcus Kim on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct White_Noise_NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
